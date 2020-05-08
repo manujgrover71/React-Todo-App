646ddeb5c67bce66f732c422df47628dc37ed925
Manuj Grover <manujgrover71@gmail.com>
2020-05-08 16:32:32 +0530

    Started project

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState('Manuj');
+
+  const [person, setPerson] = useState({ name: 'Muskan', age: 22 });
+
+  const clickHandler = () => {
+    setName("Grover");
+    setPerson({ name: "Manuj", age: 18 });
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}> 
+      <Text>My name is {name}</Text>
+      <Text>His name is {person.name} and his age is {person.age}</Text>
+      <View styles={styles.buttonContainer}>
+        <Button title='Update State' onPress={clickHandler} />
+      </View>
     </View>
   );
 }
@@ -15,5 +29,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonContainer:{
+    marginTop: 100,
   },
 });
