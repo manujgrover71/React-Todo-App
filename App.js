a1e9dd32984d5243cac035d8dd9bcb0e9c70b9ba
Manuj Grover <manujgrover71@gmail.com>
2020-05-09 13:51:18 +0530

    Created Header

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
 
@@ -11,7 +12,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      {/* Header */}
+      <Header />
       <View style={styles.content}>
         {/* to form */}
         <View style={styles.list}>
@@ -32,4 +33,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  }
 });
