0cc95697b2525a0a749afb6578324fff693da7c8
Manuj Grover <manujgrover71@gmail.com>
2020-05-09 13:37:20 +0530

    To do list starting code

--- App.js
+++ App.js
@@ -1,36 +1,28 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
 
-  const [people, setPeople] = useState([
-    { name: 'Manuj', key: '1' },
-    { name: 'Muskan', key: '2' },
-    { name: 'Rajan', key: '3' },
-    { name: 'Akshit', key: '4' },
-    { name: 'Shivam', key: '5' },
-    { name: 'Yashbeer', key: '6' },
-    { name: 'Luv', key: '7' },
-    { name: 'Mayank', key: '8' },
+  const [todos, setTodos] = useState([
+    { text: 'Buy Coffee', key: '1' },
+    { text: 'Create an app', key: '2' },
+    { text: 'Play on the switch', key: '3' },
   ]);
-
-  const pressHandler = (item) => {
-    console.log(item);
-  }
 
   return (
     <View style={styles.container}>
-      
-      <FlatList 
-        numColumns = {2}
-        data = {people}
-        renderItem = {({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-
+      {/* Header */}
+      <View style={styles.content}>
+        {/* to form */}
+        <View style={styles.list}>
+          <FlatList 
+            data = {todos}
+            renderItem = {({ item }) => (
+            <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -39,16 +31,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40, 
-    paddingHorizontal: 20,
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
-  item:{
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'skyblue',
-    fontSize: 24,
-    marginRight: 30,
-  }
 });
