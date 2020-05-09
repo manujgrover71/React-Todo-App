0da02c8b674fa179f37fbb3e1c217b95e2e32077
Manuj Grover <manujgrover71@gmail.com>
2020-05-09 17:48:04 +0530

    Added keyboard dismiss

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, Alert } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './components/header';
 import TodoItem from "./components/todoItem";
 import AddTodos from './components/addTodo';
@@ -28,27 +28,31 @@ export default function App() {
         ]
       });
     }else{
-      Alert.alert('OOPS!', 'Todos must be over 3 chars long', [{text: 'OK'}, {text: 'Cancel'}]);
+      Alert.alert('OOPS!', 'Todos must be over 3 chars long', [{text: 'OK'}]);
     }
 
     
   }
 
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        <AddTodos submitHandler={submitHandler}/>
-        <View style={styles.list}>
-          <FlatList 
-            data = {todos}
-            renderItem = {({ item }) => (
-              <TodoItem item={item} pressHandler={pressHandler}/>
-            )}
-          />
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+    }}>
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodos submitHandler={submitHandler}/>
+          <View style={styles.list}>
+            <FlatList 
+              data = {todos}
+              renderItem = {({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler}/>
+              )}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
