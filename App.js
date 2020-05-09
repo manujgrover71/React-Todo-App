c7b4ab21307c8df857829254bfd60531cf922deb
Manuj Grover <manujgrover71@gmail.com>
2020-05-09 13:11:17 +0530

    Learned Enough

--- App.js
+++ App.js
@@ -1,24 +1,36 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
 
-  const [name, setName] = useState('Manuj');
+  const [people, setPeople] = useState([
+    { name: 'Manuj', key: '1' },
+    { name: 'Muskan', key: '2' },
+    { name: 'Rajan', key: '3' },
+    { name: 'Akshit', key: '4' },
+    { name: 'Shivam', key: '5' },
+    { name: 'Yashbeer', key: '6' },
+    { name: 'Luv', key: '7' },
+    { name: 'Mayank', key: '8' },
+  ]);
 
-  const [person, setPerson] = useState({ name: 'Muskan', age: 22 });
-
-  const clickHandler = () => {
-    setName("Grover");
-    setPerson({ name: "Manuj", age: 18 });
+  const pressHandler = (item) => {
+    console.log(item);
   }
 
   return (
-    <View style={styles.container}> 
-      <Text>My name is {name}</Text>
-      <Text>His name is {person.name} and his age is {person.age}</Text>
-      <View styles={styles.buttonContainer}>
-        <Button title='Update State' onPress={clickHandler} />
-      </View>
+    <View style={styles.container}>
+      
+      <FlatList 
+        numColumns = {2}
+        data = {people}
+        renderItem = {({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.key)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+
     </View>
   );
 }
@@ -27,10 +39,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40, 
+    paddingHorizontal: 20,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-  buttonContainer:{
-    marginTop: 100,
-  },
+  item:{
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'skyblue',
+    fontSize: 24,
+    marginRight: 30,
+  }
 });
